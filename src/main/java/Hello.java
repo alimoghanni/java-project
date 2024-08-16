5975875e139e65959d362bd0a09f61f2dd8d3545
Ali Moghanni <a.moghanni@gmail.com>
2024-08-16 18:19:35 +0200

    added first files

---
+++ src/main/java/Hello.java
@@ -0,0 +1,13 @@
+// 00_Introduction/HelloWorld.java
+/*
+ * Introduction to Java
+ * This program prints "Hello, World!" to the console.
+ */
+
+public class Hello {
+    public static void main(String[] args) {
+        // Print Hello World to the console
+        System.out.println("Hello, World with Maven!");
+    }
+}
+
